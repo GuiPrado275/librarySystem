8df87d4f5cff5bca1f80f0b5ff84259202730b01
GuiPrado275 <guipradoborges275@gmail.com>
2026-07-24 15:19:13 -0300

    Create exceptions

--- src/main/java/com/guilherme/librarySystem/services/exceptions/AuthorizationException.java
+++ src/main/java/com/guilherme/librarySystem/services/exceptions/AuthorizationException.java
@@ -1,4 +1,14 @@
 package com.guilherme.librarySystem.services.exceptions;
 
-public class AuthorizationException {
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+public class AuthorizationException extends AccessDeniedException {
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+
 }
--- src/main/java/com/guilherme/librarySystem/services/exceptions/DataBindingViolationException.java
+++ src/main/java/com/guilherme/librarySystem/services/exceptions/DataBindingViolationException.java
@@ -1,4 +1,13 @@
 package com.guilherme.librarySystem.services.exceptions;
 
-public class DataBindingViolationException {
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class DataBindingViolationException extends DataIntegrityViolationException {
+
+    public DataBindingViolationException(String message) {
+        super(message);
+    }
 }
