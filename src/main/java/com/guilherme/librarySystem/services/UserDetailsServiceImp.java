76159ebc46edcc4f5cd68906e9a38b0ab2203c3f
GuiPrado275 <guipradoborges275@gmail.com>
2026-07-24 15:20:28 -0300

    Create UserService

--- src/main/java/com/guilherme/librarySystem/services/UserDetailsServiceImp.java
+++ src/main/java/com/guilherme/librarySystem/services/UserDetailsServiceImp.java
@@ -1,4 +1,29 @@
 package com.guilherme.librarySystem.services;
 
-public class UserDetailsServiceImp {
+import com.guilherme.librarySystem.models.User;
+import com.guilherme.librarySystem.repositories.UserRepository;
+import com.guilherme.librarySystem.security.UserSpringSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        User user = userRepository.findByEmail(email);
+
+        if(Objects.isNull(user)){
+            throw new UsernameNotFoundException("Email não encontrado: " + email);
+        }
+        return new UserSpringSecurity(user.getId(), user.getEmail(), user.getPassword(), user.getProfiles());
+    } //This goes to UserDetailsService and transforms the ID, username, password, and profiles into authorities.
 }
--- src/main/java/com/guilherme/librarySystem/services/UserService.java
+++ src/main/java/com/guilherme/librarySystem/services/UserService.java
@@ -1,4 +1,96 @@
 package com.guilherme.librarySystem.services;
 
+import com.guilherme.librarySystem.models.User;
+import com.guilherme.librarySystem.models.dto.UserCreateDTO;
+import com.guilherme.librarySystem.models.dto.UserUpdateDTO;
+import com.guilherme.librarySystem.models.enums.ProfileEnum;
+import com.guilherme.librarySystem.repositories.UserRepository;
+import com.guilherme.librarySystem.security.UserSpringSecurity;
+import com.guilherme.librarySystem.services.exceptions.AuthorizationException;
+import com.guilherme.librarySystem.services.exceptions.DataBindingViolationException;
+import com.guilherme.librarySystem.services.exceptions.ObjectNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(Long id){
+        UserSpringSecurity userSpringSecurity = autheticated();
+        if (Objects.isNull(userSpringSecurity)
+                || !userSpringSecurity.hasRole(ProfileEnum.ADMIN) && !id.equals(userSpringSecurity.getId())) {
+            throw new AuthorizationException("Acesso negado");
+        }
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException(
+                "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
+
+    }
+
+    @Transactional
+    public User create(User user){
+        if (this.userRepository.existsByEmail(user.getEmail())) {
+            throw new AuthorizationException("E-mail já cadastrado!");
+        }
+        user.setId(null);
+        user.setPassword(this.bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
+        user = this.userRepository.save(user);
+        return user;
+    }
+
+    @Transactional
+    public User update(User updatedUser){
+        User existingUser = findById(updatedUser.getId());
+        existingUser.setPassword(this.bCryptPasswordEncoder.encode(updatedUser.getPassword()));
+        return this.userRepository.save(existingUser);
+    }
+
+    public void delete(Long id){
+        findById(id);
+        try{
+            this.userRepository.deleteById(id);
+        } catch (Exception e){
+            throw new DataBindingViolationException("O usuário não pode ser deletado, pois ele tem empréstimos abertos!");
+        }
+    }
+
+    public static UserSpringSecurity autheticated(){
+        try{
+            return (UserSpringSecurity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+    public User fromDTO(@Valid UserCreateDTO obj) {
+        User user = new User();
+        user.setName(obj.name());
+        user.setEmail(obj.email());
+        user.setPassword(obj.password());
+        return user;
+    }
+
+    public User fromDTO(@Valid UserUpdateDTO obj){
+        User user = new User();
+        user.setId(obj.id());
+        user.setPassword(obj.password());
+        return user;
+    }
+
 }
