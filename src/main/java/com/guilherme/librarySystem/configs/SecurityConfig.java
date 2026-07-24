a02ab4fed291de2b040416ddf70fe7997d30ae7e
GuiPrado275 <guipradoborges275@gmail.com>
2026-07-24 15:20:03 -0300

    Small updates

--- src/main/java/com/guilherme/librarySystem/configs/SecurityConfig.java
+++ src/main/java/com/guilherme/librarySystem/configs/SecurityConfig.java
@@ -1,4 +1,19 @@
 package com.guilherme.librarySystem.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() { //to encrypt
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/com/guilherme/librarySystem/repositories/UserRepository.java
+++ src/main/java/com/guilherme/librarySystem/repositories/UserRepository.java
@@ -2,12 +2,12 @@ package com.guilherme.librarySystem.repositories;
 
 import com.guilherme.librarySystem.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email); //used in login (find user by email)
+    @Transactional(readOnly = true)
+    User findByEmail(String email); //to search for the user by email
 
     boolean existsByEmail(String email); //used on creation, to validate duplicate email
 }
