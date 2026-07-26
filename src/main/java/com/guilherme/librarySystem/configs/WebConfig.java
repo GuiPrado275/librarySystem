b67038a6c7da3707c036e6d05e85fa5ffbee9d71
GuiPrado275 <guipradoborges275@gmail.com>
2026-07-26 11:53:19 -0300

    Create WebConfig

--- src/main/java/com/guilherme/librarySystem/configs/WebConfig.java
+++ src/main/java/com/guilherme/librarySystem/configs/WebConfig.java
@@ -1,4 +1,20 @@
 package com.guilherme.librarySystem.configs;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //once you have one (ex: http://localhost:3000 for React/Vite, :4200 for Angular)
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+    }
 }
